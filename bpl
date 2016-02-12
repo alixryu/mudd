e520dbeeb68ea9f4213e64cf3a4eb12e160acfd5
heavenk21 <heavenk21@gmail.com>
2016-02-11 22:56:06 -0500

    Add(mudd): modularize project

--- bpl
+++ bpl
@@ -1,3 +1,5 @@
 #!/bin/bash
+PYTHONPATH="${PYTHONPATH}:$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/mudd"
+export PYTHONPATH
 # run scanner
-python3 scanner.py ${BASH_ARGV[0]}
+python3 mudd/scanner.py ${BASH_ARGV[0]}
---
+++ mudd/__init__.py
@@ -0,0 +1,43 @@
+# End-of-file
+T_EOF = 100
+# Keywords
+T_INT = 101
+T_VOID = 102
+T_STRING = 103
+T_IF = 104
+T_ELSE = 105
+T_WHILE = 106
+T_RETURN = 107
+T_WRITE = 108
+T_WRITELN = 109
+T_READ = 110
+# Symbols
+T_SEMICOL = 111
+T_COMMA = 112
+T_LBRACK = 113
+T_RBRACK = 114
+T_LCURLY = 115
+T_RCURLY = 116
+T_LPAREN = 117
+T_RPAREN = 118
+T_LES = 119
+T_LEQ = 120
+T_EQU = 121
+T_NEQ = 122
+T_GEQ = 123
+T_GRE = 124
+T_PLU = 125
+T_MIN = 126
+T_MUL = 127
+T_DIV = 128
+T_MOD = 129
+T_AND = 130
+# Assignment sign
+T_ASSIGN = 195
+# Comments
+T_LCOM = 196
+T_RCOM = 197
+# Identifiers
+T_ID = 198
+# Numbers
+T_NUM = 199
