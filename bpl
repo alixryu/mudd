f81a0e89cf5478f4bb103596e912bc06a27ae51d
heavenk21 <heavenk21@gmail.com>
2016-04-13 03:16:30 -0400

    Fix(bpl, frontdesk): run typechecker

--- bpl
+++ bpl
@@ -2,4 +2,4 @@
 PYTHONPATH="${PYTHONPATH}:$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/mudd"
 export PYTHONPATH
 # run compiler
-python3 mudd/frontdesk.py ${BASH_ARGV[0]}
+python3 mudd/frontdesk.py $@
--- mudd/frontdesk.py
+++ mudd/frontdesk.py
@@ -1,13 +1,28 @@
 import sys
 
-from mudd.parser import MuddParser, ParseError
+from mudd.parser import ParseError
+from mudd.typechecker import MuddTypeChecker, TypeCheckError
 
 
 if __name__ == '__main__':
     try:
-        parser = MuddParser(sys.argv[1])
-        parser.parse()
-        print(parser.parse_tree)
+        args = sys.argv
+        debug = False
+
+        if len(args) <= 1:
+            sys.exit(0)
+
+        if '--debug' in args:
+            debug = True
+
+        type_checker = MuddTypeChecker(args[-1], debug)
+
+        if '--tree' in args:
+            print(type_checker.parse_tree)
+
+        type_checker.top_down_pass()
+        type_checker.bottom_up_pass()
+
 
     except IndexError:
         print('[IndexError] No compilable file received as argument.')
@@ -20,3 +35,6 @@ if __name__ == '__main__':
 
     except ParseError as parse_error:
         print(parse_error)
+
+    except TypeCheckError as type_check_error:
+        print(type_check_error)
